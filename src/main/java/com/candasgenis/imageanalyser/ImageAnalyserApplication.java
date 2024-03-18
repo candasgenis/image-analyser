2fd5aa63becd6c2848e6d9f3c1ef831fae5553ca
candasgenis <68709696+candasgenis@users.noreply.github.com>
2024-03-18 23:56:43 +0300

    Kafka side inserted.

--- src/main/java/com/candasgenis/imageanalyser/ImageAnalyserApplication.java
+++ src/main/java/com/candasgenis/imageanalyser/ImageAnalyserApplication.java
@@ -3,7 +3,7 @@ package com.candasgenis.imageanalyser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.candasgenis.springbootkafkaproducer")
 public class ImageAnalyserApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/candasgenis/imageanalyser/service/ImageService.java
+++ src/main/java/com/candasgenis/imageanalyser/service/ImageService.java
@@ -1,5 +1,6 @@
 package com.candasgenis.imageanalyser.service;
 
+import com.candasgenis.springbootkafkaproducer.service.ProducerService;
 import com.example.*;
 import com.google.cloud.spring.vision.CloudVisionTemplate;
 import com.google.cloud.vision.v1.AnnotateImageResponse;
@@ -19,27 +20,39 @@ public class ImageService extends ImageServiceGrpc.ImageServiceImplBase{
 
     @Autowired private CloudVisionTemplate cloudVisionTemplate;
 
+    @Autowired private ProducerService service;
+
     @Override
     public void getImageDetail(GetImageDetailRequest request, StreamObserver<GetImageDetailResponse> responseObserver) {
         //TODO DATABASE E ISTEK ATIP IMAGE DETAIL ALMA
     }
     @Override
     public void uploadImage(UploadImageRequest request, StreamObserver<UploadImageResponse> responseObserver) {
+        //TODO BURAYI KAFKAYLA YAP, BURADA SADECE KAFKA SERVICE E URL I GONDER SONRA KAFKA SERVICE
+        // ICERIDE IMAGE I ANALIZ ETSIN VE BURAYA BEN BU IMAGE I ANALIZ ETTIM RESPONSE U DONSUN
+        // O SIRADA KAFKA IMAGE I ANALIZ EDIP DATABASE E VERIYI YAZSIN, SONRA DA ZATEN
+        // GETIMAGEFEED ILE DATABASEDEKI VERI CEKILSIN
+        // KAFKA SERVICE E DIREKT BU ANALYZEIMAGE KODUNUN AYNISINI YAZ FARK ETMEZ
 
-        //TODO BURAYI KAFKAYLA YAP
-        AnnotateImageResponse response =
-                this.cloudVisionTemplate.analyzeImage(
-                        this.resourceLoader.getResource(request.getImageUrl()), Feature.Type.FACE_DETECTION);
-
-        // This gets the annotations of the image from the response object.
-        List<FaceAnnotation> annotations = response.getFaceAnnotationsList();
-        int joyLikelihoodValue = annotations.getFirst().getJoyLikelihoodValue();
-        int sorrowLikelihoodValue = annotations.getFirst().getSorrowLikelihoodValue();
-        int angerLikelihoodValue = annotations.getFirst().getAngerLikelihoodValue();
-        int surpriseLikelihoodValue = annotations.getFirst().getSurpriseLikelihoodValue();
-        int underExposedLikelihoodValue = annotations.getFirst().getUnderExposedLikelihoodValue();
-        int blurredLikelihoodValue = annotations.getFirst().getBlurredLikelihoodValue();
-        int headwearLikelihoodValue = annotations.getFirst().getHeadwearLikelihoodValue();
+        System.out.println("Kafka log send starting...");
+        for (int i = 0; i < 10; i++) {
+            service.sendMessage(String.format("[%d] -- Hello World", i));
+        }
+        System.out.println("Kafka log sent...");
+//
+//        AnnotateImageResponse response =
+//                this.cloudVisionTemplate.analyzeImage(
+//                        this.resourceLoader.getResource(request.getImageUrl()), Feature.Type.FACE_DETECTION);
+//
+//        // This gets the annotations of the image from the response object.
+//        List<FaceAnnotation> annotations = response.getFaceAnnotationsList();
+//        int joyLikelihoodValue = annotations.getFirst().getJoyLikelihoodValue();
+//        int sorrowLikelihoodValue = annotations.getFirst().getSorrowLikelihoodValue();
+//        int angerLikelihoodValue = annotations.getFirst().getAngerLikelihoodValue();
+//        int surpriseLikelihoodValue = annotations.getFirst().getSurpriseLikelihoodValue();
+//        int underExposedLikelihoodValue = annotations.getFirst().getUnderExposedLikelihoodValue();
+//        int blurredLikelihoodValue = annotations.getFirst().getBlurredLikelihoodValue();
+//        int headwearLikelihoodValue = annotations.getFirst().getHeadwearLikelihoodValue();
 
     }
     @Override
